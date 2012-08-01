80a4d0f7cfb90b73be436ab9fae13cba5312b267
Muralidharan <muralidharan84@gmail.com>
2012-08-01 21:57:52 +0800

    Hello world Implementation in ASP.Net... Study this carefully.

---
+++ HelloWorld/Account/Login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.SessionState;
+
+namespace HelloWorld.Account
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string Username = null;
+            RegisterHyperLink.NavigateUrl = "Register.aspx?ReturnUrl=" + HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+            if (Session.Count > 0) Username = Session["UserName"].ToString();
+            if (Username == "Murali")
+            {
+                Response.Write("Session Validated");
+            }
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            Session["UserName"] = LoginUser.UserName.ToString();
+            Response.Write(LoginUser.UserName.ToString());
+        }
+    }
+}
---
+++ HelloWorld/Site.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorld
+{
+    public partial class SiteMaster : System.Web.UI.MasterPage
+    {
+        static int siteCounter = 0;
+        private static readonly Object lockobj = new Object();
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ++siteCounter;
+            lock (lockobj)
+            {
+                SiteCount.Text = siteCounter.ToString(); 
+            }
+            Response.Write("This Site has been visited" + siteCounter.ToString() + " times");
+        }
+    }
+}
